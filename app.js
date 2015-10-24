d80704ae7b4903eec62d03e866dce564bceae925
Risto Viitanen <risto.viitanen@iki.fi>
2015-10-24 20:55:15 +0300

    Add options and debug info

--- app.js
+++ app.js
@@ -3,53 +3,79 @@ var https = require('https');
 var cheerio = require('cheerio');
 var fs = require('fs');
 var url = require('url');
+var glob = require('glob');
+var colors = require('colors');
 
-const MAX_PAGES = 10;
-var hostname = 'unsplash.com';
-var image_num = 1;
-var page = 1;
+const VERSION = '0.0.1';
+
+var opt = require('node-getopt').create([
+		['', 'host', 'Custom host'],
+		['s', 'start-page=PAGE', 'Page to start from. Defaults to 1'],
+		['e', 'end-page=PAGE', 'Last page to get. Defaults to all'],
+		['h', 'help', 'Display this help'],
+		['v', 'version', 'Show version'],
+		['c', 'clean', 'Delete all data from images-directory'],
+])
+.bindHelp()
+.parseSystem();
+
+var promise = q.resolve();
+
+if (opt.options.version) {
+	console.log('Version %s'.green, VERSION);
+	process.exit(1);
+}
+else if (opt.options.clean) {
+	console.log('Cleaning images-directory...'.yellow);
+	promise = promise.then(cleanImages);
+}
+
+var hostname = opt.options.host || 'unsplash.com';
+var page = parseInt(opt.options['start-page'], 10) || 1;
+var end_page = parseInt(opt.options['end-page'], 10) || 999999;
+var image_num = (page - 1) * 20;
 
 function start() {
-	getPageNum(page++)
+	return getPageNum(page++)
 	.then(function() {
-		if (page <= MAX_PAGES) {
-			start();
+		if (page <= end_page) {
+			return start();
 		}
-	})
-	.catch(function(err) {
-		console.log(err);
-		console.trace(err);
 	});
 }
 
 function getPageNum(page) {
+	console.log('Requesting page number %d'.green, page);
 	return getPage(hostname, '/?page=' + page.toString())
-	.then(function(data) {
-		var $ = cheerio.load(data);
+	.then(function(res) {
+		var $ = cheerio.load(res.data);
 		var images = $('img[alt^="Photo By"]');
 		if (images.length === 0) {
-			console.log('Done!');
-			return exit(1);
+			return process.exit(1);
 		}
 	
 		return q.all(images.map(function() {
 			var image_url = url.parse($(this).attr('src'), true);
-			return getPage(image_url.host, image_url.path.replace('q=80&fm=jpg&w=1080&fit=max&', ''));
+			return getPage(image_url.host, image_url.pathname);
 		}));
 	})
 	.then(function(images) {
+		console.log('Got response from %d images'.blue, images.length);
 		return q.all(Object.keys(images).map(function(i) {
 			var img = images[i];
-			return q.nfcall(fs.writeFile, 'images/' + image_num++ + '.jpg', img); 
+			if (!img.name) {
+				return q.resolve();
+			}
+			return q.nfcall(fs.writeFile, 'images/' + img.name.replace(/[^a-zA-Z.\-]+/, '') + '.jpg', img.data); 
 		}));
 	})
 	.then(function() {
-		console.log('Page %d downloaded', page);
+		console.log('Page %d downloaded'.green, page);
 	});
 }
 
 function getPage(hostname, url) {
-	console.log('getPage', hostname, url);
+	console.log('Requesting https://%s'.green, hostname + url);
 	return q.promise(function(resolve, reject) {
 		https.request({
 			hostname: hostname,
@@ -68,7 +94,7 @@ function getPage(hostname, url) {
 			});
 
 			res.on('end', function() {
-				resolve(buf);
+				resolve({data: buf, name: url.substr(1)});
 			});
 
 			res.on('error', function(e) {
@@ -78,5 +104,23 @@ function getPage(hostname, url) {
 	});
 }
 
-start();
+function cleanImages() {
+	return q.nfcall(glob, 'images/*.jpg')
+	.then(function(files) {
+		return q.all(files.map(function(file) {
+			console.log('\t...%s'.red, file);
+			return q.nfcall(fs.unlink, file);
+		}));
+	});
+}
+
+promise
+.then(start)
+.then(function() {
+	console.log('All done!'.green);
+})
+.catch(function(err) {
+	console.log(err.red);
+	console.trace(err);
+});
 
